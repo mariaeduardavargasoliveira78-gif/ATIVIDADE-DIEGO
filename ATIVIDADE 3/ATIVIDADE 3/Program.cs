a3e9c504d54182be3f56dc4c654363f9df7be270
mariaeduardavargasoliveira78-gif <mariaeduardavargasoliveira78@gmail.com>
2025-10-02 17:20:29 -0300

    Add files via upload

---
+++ ATIVIDADE 3/ATIVIDADE 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite a base do retangulo: ");
+double b = double.Parse(Console.ReadLine());
+
+Console.Write(b"Digite a altura do retangulo: ");
+double h = double.Parse(Console.ReadLine());
+
+double area = b * h;
+
+Console.WriteLine($"A area do retangulo é`:);
+
