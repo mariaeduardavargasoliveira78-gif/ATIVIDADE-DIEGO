f2b09871ad79abd1d57f83d8bf8ae101a18c5fa5
mariaeduardavargasoliveira78-gif <mariaeduardavargasoliveira78@gmail.com>
2025-10-02 17:21:15 -0300

    Add files via upload

---
+++ ATIVIDADE 4/ATIVIDADE 4/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Digite sua idade em anos: ");
+int anos = int.Parse(Console.ReadLine());
+
+Console.Write(anos"Digite meses: ");
+int meses = int.Parse(Console.ReadLine());
+
+Console.Write(meses "digite dias: ");
+int dias = int.Parse(Console.ReadLine());
+
+int totaldias + anos *365 + meses *30 + dias;
+
+Console.WriteLine($"A idade em dias é´: {totaldias}");
---
+++ ATIVIDADE 9/ATIVIDADE 9/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("digite a quatidade de maçãs compradas: ");
+
+int quantidades = int.Parse(Console.ReadLine());
+
+// se forem menos de 12 maçãs,preço é 1,30, caso contrário,1,00.
+decimal precoUnitario * quantidade;
+decimal total = precoUnitario * quantidade;
+
+Console.WriteLine();
+Console.WriteLine($"quantidade: {quantidades}");
+Console.WriteLine($"preço unitario: {precoUnitario.ToString("(c")}");
+Console.WriteLine($"custo total de compras: {total.ToString("c")}");
+
+
+Console.WriteLine("/nPressione Enter para sair...");
+Console.ReadLine();
